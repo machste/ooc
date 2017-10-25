87d86fadce0dbd954d45cd5ecdccc7d928c5b256
Stefan Mächler <stefan.maechler@gmail.com>
2017-10-25 21:52:39 +0200

    Use File class for stdoutput object

--- src/util/print.c
+++ src/util/print.c
@@ -1,3 +1,25 @@
+#include <util/file.h>
+
+#include <util/print.h>
+
+static file _stdoutput = { .obj = { .cls = NULL } };
+
+static file *_get_stdoutput(void)
+{
+    file *out;
+
+    if(_stdoutput.obj.cls == NULL) {
+        out = init(&_stdoutput, File, "<stdout>");
+        out->file = stdout;
+    } else {
+        out = &_stdoutput;
+    }
+    return out;
+}
+
 void print(void *obj)
 {
+    file *out = _get_stdoutput();
+    put(obj, out);
+    write(out, "\n", 1);
 }
